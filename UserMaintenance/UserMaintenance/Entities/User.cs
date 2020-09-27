365b15b28ace27462f85f01558de819fca4e1d68
virag9354 <repovszki.v@gmail.com>
2020-09-27 11:52:20 +0200

    .

---
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserMaintenance.Entities
+{
+    public class User
+    {
+
+            public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName);
+            }
+        }
+
+        // Ugyanaz a FullName property kompaktabb formában is írható
+        /*
+        public string FullName
+            => string.Format(
+                "{0} {1}",
+                LastName,
+                FirstName);
+        */
+         
+    }
+}
+
