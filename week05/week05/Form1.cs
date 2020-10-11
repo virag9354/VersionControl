9d0898d60d525199c11c5c06e0a675a5c76c4c66
virag9354 <repovszki.v@gmail.com>
2020-10-11 20:37:20 +0200

    otos

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.Entities;
 
 namespace week05
 {
     public partial class Form1 : Form
     {
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
