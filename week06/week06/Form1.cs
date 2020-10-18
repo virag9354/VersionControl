485e0ce4e336c51e79d82b43067abceec17df67f
virag9354 <repovszki.v@gmail.com>
2020-10-18 21:39:51 +0200

    4

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -33,7 +33,31 @@ namespace week06
 
             var result = response.GetExchangeRatesResult;
             dataGridView1.DataSource = Rates;
-            
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+             
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+
+
         }
     }
 }
