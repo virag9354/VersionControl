afab60aa3112cebc455381050be58ea96ceadb49
virag9354 <repovszki.v@gmail.com>
2020-11-15 21:29:35 +0100

    7

--- week08/week08/Entities/Car.cs
+++ week08/week08/Entities/Car.cs
@@ -4,16 +4,16 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 using week08.Abstractions;
 
 namespace week08.Entities
 {
-    public class Ball: Toy
+    public class Car: Toy
     {
         protected override void DrawImage(Graphics g)
         {
-            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
         }
     }
 }
