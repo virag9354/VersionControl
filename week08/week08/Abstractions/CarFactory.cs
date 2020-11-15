8ffa750addcb8274f97e35d459b4576f7a4db756
virag9354 <repovszki.v@gmail.com>
2020-11-15 21:40:01 +0100

    9.2

---
+++ week08/week08/Abstractions/CarFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week08.Entities;
+
+namespace week08.Abstractions
+{
+    public class CarFactory: IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Car();
+        }
+    }
+}
